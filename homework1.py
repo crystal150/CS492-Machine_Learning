d5cfe184ef52b1e5a9e16398471e2913ecf80e7e
Jaryong_Lee <crystal00@kaist.ac.kr>
2016-10-13 21:54:15 +0900

    split 'split_set' function to two function

--- homework1.py
+++ homework1.py
@@ -3,15 +3,27 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 import random
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def load_data ( file_name ):
     print "Load data from %s ..." % file_name 
 
     lines = [line.strip().split() for line in open(file_name).readlines()]
     data_set = [([line[0]] + [ele.split(":") for ele in line[1:]]) for line in lines]
+    # data_set [ ['24', ('1', '10'), ('2', '353'), ... ('13', '123')], [...], ... [...] ]
 
     return data_set
+
+def csr_matrix_from_indices ( x, dim_y, dim_x ):
+    row = []
+    col = []
+    data = []
+    for i in range(len(x)):
+        row.extend([i for ele in x[i]])
+        col.extend([int(ele[0])-1 for ele in x[i]])
+        data.extend([float(ele[1]) for ele in x[i]])
+    sparse = csr_matrix ( (data, (row, col)), shape = (dim_y, dim_x), dtype = float)
+    return np.mat(sparse.toarray())
 
 def split_set ( num, ratio, data_set ):
     print "Split data set to train and test set randomly ..."
@@ -23,22 +35,8 @@ def split_set ( num, ratio, data_set ):
         num_training = int (ratio * len(data_set))
         splited_set = [data_set[:num_training], data_set[num_training:]]
         splited_y = [np.array([line[0] for line in set], dtype=float) for set in splited_set]
-        splited_x = []
-
         splited_x_temp = [[line[1:] for line in set] for set in splited_set]
-        for j in range(len(splited_x_temp)):
-            row = []
-            col = []
-            data = []
-            for i in range(len(splited_x_temp[j])):
-                for ele in splited_x_temp[j][i]:
-                    row.append (i)
-                    col.append (int(ele[0]) - 1)
-                    data.append ((float(ele[1])))
-            sparse = csr_matrix ( (data, (row, col)), shape = (len(splited_y[j]), argnum), dtype =
-                    float)
-            splited_x.append(np.mat(sparse.toarray()))
-
+        splited_x = [csr_matrix_from_indices (x, len(x), argnum) for x in splited_x_temp]
         splited_yx_train = [splited_y[0], splited_x[0]]
         splited_yx_test = [splited_y[1], splited_x[1]]
         full_splited_yx.append([splited_yx_train, splited_yx_test])
@@ -55,7 +53,7 @@ def print_with_bar ( print_temp ):
 def cost_function_linear( y, x, beta ):
     # print "Calculate cost ..."
 
-    temp_square = (beta.dot(x.transpose())-y).dot(x)
+    temp_square = (beta.dot(x.transpose())-y)
     return np.sum(temp_square.transpose().dot(temp_square))/(2*y.shape[0])
 
 def anlaytic_solution ( y, x ):
@@ -74,7 +72,7 @@ def gradient_descent ( y, x, beta, type='linear' ):
 def prediction_error_linear ( y, x, beta ):
     # print "Calculate prediction error ..."
 
-    return np.sum(np.abs(y - beta.dot(x.transpose())))
+    return np.sum(np.abs(y - beta.dot(x.transpose())))/y.shape[0]
 
 def sigmoid ( z ):
     return 1 / (1 + np.exp(-z))
@@ -88,9 +86,9 @@ def cost_function_logistic ( y, x, beta ):
 def prediction_error_logistic ( y, x, beta ):
     # print "Calculate prediction error ..."
 
-    return np.sum(np.abs(y - sigmoid(beta.dot(x.transpose()))))
+    return np.sum(np.abs(y - sigmoid(beta.dot(x.transpose()))))/y.shape[0]
 
-def backtracking_line_search ( y, x, beta, step_size, back):
+def backtracking_line_search ( y, x, beta, step_size, back, ratio):
     # print "Calculate gradient descent ..."
     gradient = gradient_descent (y, x, beta, 'logistic')
     while True:
@@ -98,7 +96,7 @@ def backtracking_line_search ( y, x, beta, step_size, back):
         cost_expected = cost_function_logistic (y, x, beta) + back * step_size * np.sum(gradient.transpose().dot(gradient))
         if cost_actual < cost_expected:
             break
-        step_size /= 2
+        step_size *= ratio
 
     return beta - step_size * gradient
 
@@ -107,10 +105,11 @@ if __name__ == "__main__":
 
     num_set = 10
     iteration = 100
-    linear_gradient_step_size = 0.1
-    logistic_gradient_step_size = 5
+    linear_gradient_step_size = 0.15
+    logistic_gradient_step_size = 1
     logistic_backtracking_step_size = 10
     backtracking_hyperparameter = 0.01
+    backtracking_ratio = 0.5
 
     data_set = load_data ("housing_scale")
     splited_data_set = split_set (num_set, 0.8, data_set)
@@ -140,10 +139,10 @@ if __name__ == "__main__":
         # print "error : %s" % error
 
         errors_linear_analytic.append(error)
-        if first:
-            cost_analytic = iteration * [cost_function_linear (test_set_y, test_set_x,
-                beta_linear_analytic)]
-            first = False
+        cost_analytic.append(cost_function_linear (test_set_y, test_set_x, beta_linear_analytic))
+
+    cost_analytic = iteration * [np.mean(cost_analytic)]
+        
     print_with_bar ( "Prediction error: %s by %s splited set --- Analytic Solution" % (np.mean(errors_linear_analytic), num_set) )
 
 
@@ -179,10 +178,10 @@ if __name__ == "__main__":
         errors_linear_gradient.append(error)
     print_with_bar ("Prediction error: %s by %s iteration %s splited set --- Gradient Descent" % (np.mean(errors_linear_gradient), iteration, num_set) )
 
-#    plt_iter = range(iteration)
-#    plt.plot(plt_iter, cost_analytic, plt_iter, cost_gradient)
-#    plt.ylabel('Cost')
-#    plt.show()
+    plt_iter = range(iteration)
+    plt.plot(plt_iter, cost_analytic, plt_iter, cost_gradient)
+    plt.ylabel('Cost')
+    plt.show()
 
 
     data_set = load_data ("a3a")
@@ -224,10 +223,10 @@ if __name__ == "__main__":
         errors_logistic_gradient.append(error)
     print_with_bar ("Prediction error: %s by %s iteration %s splited set --- Gradient Descent" % (np.mean(errors_logistic_gradient), iteration, num_set) )
     
-#    plt_iter = range(iteration)
-#    plt.plot(plt_iter, cost_gradient)
-#    plt.ylabel('Cost')
-#    plt.show()
+    plt_iter = range(iteration)
+    plt.plot(plt_iter, cost_gradient)
+    plt.ylabel('Cost')
+    plt.show()
 
 
 
@@ -250,7 +249,7 @@ if __name__ == "__main__":
         for _ in range(iteration):
             beta_logistic_backtracking = backtracking_line_search (train_set_y, train_set_x,
                     beta_logistic_backtracking, logistic_backtracking_step_size,
-                    backtracking_hyperparameter)
+                    backtracking_hyperparameter, backtracking_ratio)
             if first: cost_gradient.append (cost_function_logistic (test_set_y, test_set_x,
                 beta_logistic_backtracking))
 
@@ -262,7 +261,7 @@ if __name__ == "__main__":
         errors_logistic_backtracking.append(error)
     print_with_bar ("Prediction error: %s by %s iteration %s splited set --- Backtracking Line Search" % (np.mean(errors_logistic_backtracking), iteration, num_set) )
     
-#    plt_iter = range(iteration)
-#    plt.plot(plt_iter, cost_gradient)
-#    plt.ylabel('Cost')
-#    plt.show()
+    plt_iter = range(iteration)
+    plt.plot(plt_iter, cost_gradient)
+    plt.ylabel('Cost')
+    plt.show()
